fffc4a4cb1c52381f0b10696717110914841e944
J.M <jonnel.vidal@gmail.com>
2019-12-02 19:12:53 +0100

    Creación prueba componente Angular

--- Mvsix-Project/src/app/app.module.ts
+++ Mvsix-Project/src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { PruebaComponenteComponent } from './prueba-componente/prueba-componente.component';
 
 @NgModule({
   declarations: [
     
-  ],
+  PruebaComponenteComponent],
   imports: [
     BrowserModule
   ],
