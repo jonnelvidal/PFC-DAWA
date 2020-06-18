01bfb54513c8d3a828a06ce45c44a1a8ddc8329c
J.M <jonnel.vidal@gmail.com>
2020-06-18 05:12:02 +0200

    Implementación Validación formulario subir tema

--- Mvsix-Project/src/app/tema/tema.component.ts
+++ Mvsix-Project/src/app/tema/tema.component.ts
@@ -3,6 +3,7 @@ import { Tema } from '../../entities/tema';
 import { ApiService } from '../api.service';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../auth.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tema',
@@ -13,8 +14,9 @@ export class TemaComponent implements OnInit {
   p: number = 1;
   temas: Tema[];
   subirTema: Boolean = false;
+  formularioTema: FormGroup;
   temaSeleccionado: Tema ={idTema: null, nombre: null, archivoTema: null, duracion: null, imagen: null, nombreArtista: null, valoracion: null}
-  constructor(private apiService: ApiService, private auth: AuthService, private httpx: HttpClient) { }
+  constructor(private apiService: ApiService, private auth: AuthService, private httpx: HttpClient, private _formBuilder: FormBuilder) { }
   onSelectFile(event){
     const fd = new FormData();
     fd.append('image', event.target.files[0], event.target.files[0].name);
@@ -23,6 +25,7 @@ export class TemaComponent implements OnInit {
         console.log("event.target.files[0]") 
       });
   }
+  get datosTema() { return this.formularioTema.controls; }
   createOrUpdateTema(form){
     
       this.temaSeleccionado = form.value;
@@ -33,7 +36,7 @@ export class TemaComponent implements OnInit {
       });
   }
   mostrarFormularioTema(){
-    
+    this.subirTema = true;
   }
   seleccionarTema(tema: Tema){
     this.temaSeleccionado = tema;
@@ -48,10 +51,19 @@ export class TemaComponent implements OnInit {
     
   }
   ngOnInit() {
+    this.formularioTema = this._formBuilder.group({
+      nombre: ['', Validators.required],
+      archivoTema: ['', Validators.required],
+      nombreArtista: ['', Validators.required],
+      duracion: ['', Validators.required],
+      valoracion: ['', Validators.required],
+      imagen: ['', Validators.required]
+    });
     this.apiService.readTemasUsuario(this.auth.getUsuario()).subscribe((temas: Tema[])=>{
       this.temas = temas;
       console.log(this.temas)
     });
+    
   }
 
 }
